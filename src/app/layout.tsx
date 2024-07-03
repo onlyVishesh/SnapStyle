40c3ee0d44d771f8a54642f611027fc124031b8a
onlyVishesh <visheshmaurya14@gmail.com>
2024-07-03 23:27:03 +0530

    added options on design page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/ui/Providers";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -21,7 +22,9 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         <main className="flex min-h-[calc(100vh-3.5rem-1px)] flex-col">
-          <div className="flex h-full flex-1 flex-col">{children}</div>
+          <div className="flex h-full flex-1 flex-col">
+            <Providers>{children}</Providers>
+          </div>
           <Footer />
         </main>
       </body>
---
+++ src/config/products.ts
@@ -0,0 +1,12 @@
+export const PRODUCT_PRICES = {
+  material: {
+    silicone: 0,
+    polycarbonate: 400_00,
+  },
+  finish: {
+    smooth: 0,
+    textured: 200_00,
+  },
+} as const;
+
+export const BASE_PRICE = 1000_00;
